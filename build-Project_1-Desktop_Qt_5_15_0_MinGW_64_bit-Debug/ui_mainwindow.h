abcb168979ee544c502cc1c17332ce838395c502
Hannselthill Camacho <45092062+Hannsel101@users.noreply.github.com>
2020-07-06 08:10:51 -0700

    Created the project in QT Creator 4.12.3 (Community)

---
+++ build-Project_1-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,97 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QLabel *Input_Label;
+    QLineEdit *Input_LineEdit;
+    QPushButton *InputSearch_Button;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        widget = new QWidget(centralwidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(20, 10, 321, 25));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        Input_Label = new QLabel(widget);
+        Input_Label->setObjectName(QString::fromUtf8("Input_Label"));
+
+        horizontalLayout->addWidget(Input_Label);
+
+        Input_LineEdit = new QLineEdit(widget);
+        Input_LineEdit->setObjectName(QString::fromUtf8("Input_LineEdit"));
+
+        horizontalLayout->addWidget(Input_LineEdit);
+
+        InputSearch_Button = new QPushButton(widget);
+        InputSearch_Button->setObjectName(QString::fromUtf8("InputSearch_Button"));
+
+        horizontalLayout->addWidget(InputSearch_Button);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        Input_Label->setText(QCoreApplication::translate("MainWindow", "Input File", nullptr));
+        Input_LineEdit->setInputMask(QString());
+        Input_LineEdit->setText(QString());
+        Input_LineEdit->setPlaceholderText(QCoreApplication::translate("MainWindow", "Example: C:/user/file.txt", nullptr));
+        InputSearch_Button->setText(QCoreApplication::translate("MainWindow", "...", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
